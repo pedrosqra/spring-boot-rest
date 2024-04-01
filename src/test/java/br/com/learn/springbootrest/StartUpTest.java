c0ff1fdac59d5b37c8b88ba04c035b90693ed65a
Pedro Lima <pedro.lima@ccc.ufcg.edu.br>
2024-03-31 21:17:04 -0300

    Setting up the Spring Boot project

---
+++ src/test/java/br/com/learn/springbootrest/StartUpTest.java
@@ -0,0 +1,13 @@
+package br.com.learn.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
