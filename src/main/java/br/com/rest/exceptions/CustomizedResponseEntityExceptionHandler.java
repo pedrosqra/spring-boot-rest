464abf2aeb1356ff298d82cf7843278b3b5e828b
Pedro Lima <pedro.lima@ccc.ufcg.edu.br>
2024-06-10 16:41:11 -0300

    Add JSON serialization, content negotiation

---
+++ src/main/java/br/com/rest/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,34 @@
+package br.com.rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ResponseException> handleAllExceptions(Exception ex, WebRequest request) {
+        ResponseException responseException = new ResponseException(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(responseException, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ResponseException> handleNotFoundExceptions(Exception ex, WebRequest request) {
+        ResponseException responseException = new ResponseException(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(responseException, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`first_name`, `last_name`, `address`, `gender`) VALUES
+  ('Beyoncé', 'Knowles', 'Indian Creek Village, Miami, FL', 'Female'),
+  ('Taylor', 'Swift', 'Nashville, TN', 'Female'),
+  ('Emma', 'Watson', 'London, England', 'Female'),
+  ('Oprah', 'Winfrey', 'The Promontory, Montecito, CA', 'Female'),
+  ('Natalie', 'Portman', 'Los Feliz, Los Angeles, CA', 'Female');
